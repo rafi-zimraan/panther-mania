74b3539dc5861c7556dc96a3715f5ce37b385da7
Radiant-F <radiantfadilah0@gmail.com>
2023-02-20 15:44:00 +0700

    import images

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import ImgBGDefault from './images/defaultbackground.png';
+import ImgPMCar from './images/panther-mania-car.png';
+import ImgBgOnBoarding from './images/onboarding.jpg';
+import ImgBgPlain from './images/emptybackground.jpeg';
+
+export {ImgBGDefault, ImgPMCar, ImgBgOnBoarding, ImgBgPlain};

3c5006791ce6f5221fc7472025741a145daa8b11
Radiant-F <radiantfadilah0@gmail.com>
2023-02-20 15:45:55 +0700

    import screens

---
+++ src/screens/index.js
@@ -0,0 +1,8 @@
+import Splash from './SplashScreen/Splash';
+import SignIn from './SignIn/SignIn';
+import SignUp from './SignUp/SignUp';
+import Home from './Home/Home';
+import AuthMethod from './AuthMethod/AuthMethod';
+import OnBoarding from './OnBoarding/OnBoarding';
+
+export {Splash, SignIn, SignUp, Home, AuthMethod, OnBoarding};

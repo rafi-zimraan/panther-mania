231ae9f5d844fd50d367bfdff71fcc2cb4910ee6
Radiant-F <radiantfadilah0@gmail.com>
2023-03-10 19:17:50 +0700

    add activity indicator & life cycle

--- src/screens/SplashScreen/Splash.js
+++ src/screens/SplashScreen/Splash.js
@@ -1,30 +1,42 @@
-import {Button, Image, StatusBar, StyleSheet, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Button,
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React, {useEffect} from 'react';
 import {ImgBGDefault, ImgPMCar} from '../../assets';
 import useOrientation from '../../utils/useOrientation';
 import EncryptedStorage from 'react-native-encrypted-storage';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchSignIn} from '../../features/Auth/services/signInServices';
 
 export default function Splash({navigation}) {
+  const dispatch = useDispatch();
+  const {status_signin: status} = useSelector(state => state.auth);
   const {height, width} = useOrientation();
 
   useEffect(() => {
+    async function refreshSession() {
+      const credential = await EncryptedStorage.getItem('user_credential');
+      if (credential) {
+        const {email, password} = JSON.parse(credential);
+        console.log(email, password);
+        dispatch(fetchSignIn({email, password, navigation, splash: true}));
+      } else {
+        const firstInit = await EncryptedStorage.getItem('first_init');
+        setTimeout(() => {
+          firstInit
+            ? navigation.replace('AuthMethod')
+            : navigation.replace('OnBoarding');
+        }, 1500);
+      }
+    }
     refreshSession();
   }, []);
-
-  async function refreshSession() {
-    const credential = await EncryptedStorage.getItem('user_credential');
-    if (credential) {
-      const {email, password} = JSON.parse(credential);
-      navigation.replace('Home');
-    } else {
-      const firstInit = await EncryptedStorage.getItem('first_init');
-      setTimeout(() => {
-        firstInit
-          ? navigation.replace('AuthMethod')
-          : navigation.replace('OnBoarding');
-      }, 2000);
-    }
-  }
 
   return (
     <View style={styles.container}>
@@ -33,6 +45,11 @@ export default function Splash({navigation}) {
         style={{width, height, position: 'absolute'}}
       />
       <Image source={ImgPMCar} />
+      <ActivityIndicator
+        color={'white'}
+        animating={status == 'pending'}
+        size={'large'}
+      />
     </View>
   );
 }

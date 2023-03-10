a22f5a0fc1439c44cf3302b06f04213bc34e765c
Radiant-F <radiantfadilah0@gmail.com>
2023-03-10 18:10:40 +0700

    update assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,27 @@ import ImgBGDefault from './images/defaultbackground.png';
 import ImgPMCar from './images/panther-mania-car.png';
 import ImgBgOnBoarding from './images/onboarding.jpg';
 import ImgBgPlain from './images/emptybackground.jpeg';
+import ImgAppLogo from './images/app-logo.png';
+import ImgShirt from './images/shirt-sleeve.png';
+import ImgCommunity from './images/community.jpg';
 
-export {ImgBGDefault, ImgPMCar, ImgBgOnBoarding, ImgBgPlain};
+import IconWhatsApp from './icons/whatsapp.png';
+import IconCalendar from './icons/timetable.png';
+import IconProduct from './icons/box.png';
+import IconSOS from './icons/sos.png';
+import IconUser from './icons/user.png';
+
+export {
+  ImgCommunity,
+  ImgShirt,
+  ImgBGDefault,
+  ImgPMCar,
+  ImgBgOnBoarding,
+  ImgBgPlain,
+  ImgAppLogo,
+  IconWhatsApp,
+  IconCalendar,
+  IconProduct,
+  IconSOS,
+  IconUser,
+};

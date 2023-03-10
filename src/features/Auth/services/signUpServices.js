14d38384112f145211bb7e902f68dacc472b6cf2
Radiant-F <radiantfadilah0@gmail.com>
2023-03-10 18:13:33 +0700

    rename arg & update redux action

--- src/features/Auth/services/signUpServices.js
+++ src/features/Auth/services/signUpServices.js
@@ -1,18 +1,18 @@
 import {createAsyncThunk} from '@reduxjs/toolkit';
-import {SetUserSignUp} from '../../../redux/slices/authSlice';
+import {SetUserCredential} from '../../../redux/slices/authSlice';
 import {getUserData, postSignUp} from '../../../utils/services';
 
 // const handleCatch
 
 export const fetchSignUp = createAsyncThunk(
   'fetchSignUp',
-  async (formData, {dispatch}) => {
+  async (multiPart, {dispatch}) => {
     try {
-      const {data: response} = await postSignUp(formData);
+      const {data: response} = await postSignUp(multiPart);
       const {data, API, access_token} = response;
       if (data == 'User Berhasil Registrasi') {
         const {data} = await getUserData(API.id, access_token);
-        dispatch(SetUserSignUp({access_token, user_data: data.API[0]}));
+        dispatch(SetUserCredential({access_token, user_data: data.API[0]}));
       } else console.log('gagal sign up:', response);
       return response;
     } catch (error) {

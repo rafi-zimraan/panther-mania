5a5c6cfbac141f68b75754c5e30bb5bf069b16b7
Radiant-F <radiantfadilah0@gmail.com>
2023-03-10 18:47:05 +0700

    slicing header, menu, product

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,12 +1,34 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {Header, Menu, Products} from '../../features/Home';
+import {BackgroundImage} from '../../components';
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={{flex: 1}}>
+      <BackgroundImage />
+      <ScrollView>
+        <View style={styles.container}>
+          <Header />
+          <View style={styles.containerContent}>
+            <Menu />
+            <Products />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  containerContent: {
+    backgroundColor: 'white',
+    borderTopRightRadius: 50,
+    borderTopLeftRadius: 50,
+  },
+  container: {
+    width: '100%',
+    alignSelf: 'center',
+    maxWidth: 480,
+  },
+});

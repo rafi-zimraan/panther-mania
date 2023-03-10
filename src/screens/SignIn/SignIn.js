8b90561faf65e5a35aa136c75081db612c26274a
Radiant-F <radiantfadilah0@gmail.com>
2023-03-10 19:16:27 +0700

    add signin

--- src/screens/SignIn/SignIn.js
+++ src/screens/SignIn/SignIn.js
@@ -4,40 +4,74 @@ import {
   View,
   Image,
   TouchableNativeFeedback,
+  ScrollView,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {ImgBgPlain, ImgPMCar} from '../../assets';
 import useOrientation from '../../utils/useOrientation';
-import {FormInput} from '../../features/Auth';
+import {ButtonSubmit, FormInput} from '../../features/Auth';
 import {Gap} from '../../components';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchSignIn} from '../../features/Auth/services/signInServices';
 
-export default function SignIn() {
+export default function SignIn({navigation}) {
+  const dispatch = useDispatch();
+  const {status_signin: status} = useSelector(state => state.auth);
   const {width, height} = useOrientation();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const disableButton =
+    email == '' ||
+    !email.includes('@') ||
+    !email.includes('.') ||
+    password == '' ||
+    status == 'pending';
+
   return (
-    <View style={styles.container}>
-      <Image
-        source={ImgBgPlain}
-        style={{width, height, position: 'absolute'}}
-      />
-      <Image source={ImgPMCar} />
-      <FormInput placeholder="contoh@email.com" />
-      <Gap height={15} />
-      <FormInput placeholder="Kata Sandi" password iconName="lock" />
-      <Gap height={15} />
-      <TouchableNativeFeedback>
-        <View>
-          <Text>Masuk</Text>
-        </View>
-      </TouchableNativeFeedback>
+    <View style={{flex: 1}}>
+      <View style={styles.container}>
+        <Image
+          source={ImgBgPlain}
+          style={{width, height, position: 'absolute'}}
+        />
+        <Image
+          source={ImgPMCar}
+          style={{position: 'absolute', opacity: 0.25}}
+        />
+        <FormInput
+          placeholder="contoh@email.com"
+          keyboardType={'email-address'}
+          autoCapitalize={'none'}
+          onChangeText={setEmail}
+        />
+        <Gap height={15} />
+        <FormInput
+          placeholder="Kata Sandi"
+          password
+          iconOverride={'lock'}
+          onChangeText={setPassword}
+        />
+        <Gap height={15} />
+        <ButtonSubmit
+          title="MASUK"
+          disabled={disableButton}
+          loading={status == 'pending'}
+          onPress={() => dispatch(fetchSignIn({email, password, navigation}))}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+    flex: 1,
+    width: '100%',
+    maxWidth: 420,
+    alignSelf: 'center',
   },
 });

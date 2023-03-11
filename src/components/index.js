ce2b9e52012173a6cf0003df266073b4f1e0be75
Radiant-F <radiantfadilah0@gmail.com>
2023-03-11 15:47:16 +0700

    import header

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import BackgroundImage from './screens/BackgroundImage';
 import Gap from './screens/Gap';
+import Header from './screens/Header';
 
-export {Gap, BackgroundImage};
+export {Gap, BackgroundImage, Header};

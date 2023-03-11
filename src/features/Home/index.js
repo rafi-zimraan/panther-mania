4a47fb08f64ab96ac02e2bb0ae1310e2fbb31e1b
Radiant-F <radiantfadilah0@gmail.com>
2023-03-11 15:47:31 +0700

    import agendas & home header

--- src/features/Home/index.js
+++ src/features/Home/index.js
@@ -1,5 +1,6 @@
-import Header from './components/Header';
+import Agendas from './components/Agendas';
+import HomeHeader from './components/Header';
 import Menu from './components/Menu';
 import Products from './components/Products';
 
-export {Header, Menu, Products};
+export {Menu, Products, Agendas, HomeHeader};

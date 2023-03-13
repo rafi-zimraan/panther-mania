396753a932b68cefa92ca685005f2fb65b23d55c
Radiant-F <radiantfadilah0@gmail.com>
2023-03-13 16:33:01 +0700

    update endpoints

--- src/utils/services.js
+++ src/utils/services.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {API_KEY} from '@env';
 
 const config = access => ({
-  header: {
+  headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
     Authorization: `Bearer ${access}`,
@@ -18,8 +18,9 @@ const configMultipart = access => ({
 });
 
 export const postSignUp = formData =>
-  axios.post(`${API_KEY}/auth/signup`, formData, configMultipart());
+  axios.post(`${API_KEY}/register`, formData, config());
 export const postSignIn = formData =>
-  axios.post(`${API_KEY}/auth/signin`, formData, config());
-export const getUserData = (id, access) =>
-  axios.get(`${API_KEY}/dataprofiles/${id}`, config(access));
+  axios.post(`${API_KEY}/auth`, formData, config());
+export const getUserData = access => axios.get(`${API_KEY}/me`, config(access));
+export const postSignOut = access =>
+  axios.post(`${API_KEY}/logout`, config(access));

54f8cf6963e5e38bf301b58d117c141dd27478f1
Radiant-F <radiantfadilah0@gmail.com>
2023-03-28 15:52:25 +0700

    test google map marker

--- src/screens/__test__/LibsTest.test.js
+++ src/screens/__test__/LibsTest.test.js
@@ -1,33 +1,56 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Linking} from 'react-native';
 import React from 'react';
-import axios from 'axios';
-import formExample from '../SignUp/formExample';
+import MapView, {PROVIDER_GOOGLE, Callout, Marker} from 'react-native-maps';
 
 export default function LibsTest() {
-  let multiPart = new FormData();
-  let json = formExample;
-
-  for (let p in json) multiPart.append(p, json[p]);
-
-  multiPart.append('profile', formExample.profile);
-  multiPart.append('ktp', formExample.ktp);
-  multiPart.append('bukti_tf', formExample.bukti_tf);
-  multiPart.append('sim', formExample.sim);
-  multiPart.append('stnk', formExample.stnk);
-  axios
-    .put('https://panther-mania.id/api/v1/update', multiPart, {
-      headers: {
-        Authorization: `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvcGFudGhlci1tYW5pYS5pZFwvYXBpXC92MVwvYXV0aCIsImlhdCI6MTY3OTkwMzI3OCwiZXhwIjoxNjc5OTA2ODc4LCJuYmYiOjE2Nzk5MDMyNzgsImp0aSI6InljNTNRMG5NRUF4cDdNQnkiLCJzdWIiOjMxMzcsInBydiI6IjIzYmQ1Yzg5NDlmNjAwYWRiMzllNzAxYzQwMDg3MmRiN2E1OTc2ZjcifQ.w4FpKOEsT-PoQygI_9YxUPw5VoMznRXt5a9gpWDXbWg`,
-      },
-    })
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(err => console.log(err));
-
+  const userLocations = [
+    {
+      latitude: -7.995307,
+      longitude: 110.296256,
+      whatsapp: 6285157439660,
+      user_name: 'Radiant',
+    },
+    {
+      latitude: -7.997018,
+      longitude: 110.297909,
+      whatsapp: 6285695078232,
+      user_name: 'Jujun',
+    },
+    {
+      latitude: -7.996729,
+      longitude: 110.294103,
+      whatsapp: 6281931314133,
+      user_name: 'Mamah',
+    },
+  ];
   return (
-    <View>
-      <Text>LibsTest</Text>
+    <View style={{flex: 1}}>
+      <MapView
+        showsCompass
+        showsMyLocationButton
+        style={{flex: 1}}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -7.9964694,
+          longitude: 110.295591,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}>
+        {userLocations.map((v, i) => (
+          <Marker
+            onSelect={() => console.log('on select')}
+            onCalloutPress={() => console.log('on callout press')}
+            onPress={async () =>
+              await Linking.openURL(`https://wa.me/${v.whatsapp}`)
+            }
+            onDeselect={() => console.log('on deselect')}
+            key={i}
+            coordinate={{
+              latitude: v.latitude,
+              longitude: v.longitude,
+            }}></Marker>
+        ))}
+      </MapView>
     </View>
   );
 }

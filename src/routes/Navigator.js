c12af4a339521dd3f570ae3463c47aec1849dd1a
Radiant-F <radiantfadilah0@gmail.com>
2023-03-29 15:12:06 +0700

    update statusbar config & add edit user profile

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -5,6 +5,7 @@ import {
   Agenda,
   AgendaDetail,
   AuthMethod,
+  EditUserProfile,
   Home,
   LibsTest,
   OnBoarding,
@@ -20,16 +21,31 @@ import RNBootSplash from 'react-native-bootsplash';
 
 const Stack = createNativeStackNavigator();
 export default function Navigator() {
+  const statusBarConfig = (barStyle = 'dark') => ({
+    statusBarTranslucent: true,
+    statusBarColor: 'transparent',
+    statusBarStyle: barStyle,
+  });
+
   return (
     <NavigationContainer onReady={() => RNBootSplash.hide()}>
       <Stack.Navigator
         screenOptions={{
+          ...statusBarConfig(),
           headerShown: false,
         }}>
         {/* <Stack.Screen component={LibsTest} name={'LibsTest'} /> */}
-        <Stack.Screen component={Splash} name={'Splash'} />
+        <Stack.Screen
+          component={Splash}
+          name={'Splash'}
+          options={{...statusBarConfig('light')}}
+        />
         <Stack.Screen component={OnBoarding} name={'OnBoarding'} />
-        <Stack.Screen component={AuthMethod} name={'AuthMethod'} />
+        <Stack.Screen
+          component={AuthMethod}
+          name={'AuthMethod'}
+          options={{...statusBarConfig('light')}}
+        />
         <Stack.Screen component={SignIn} name={'SignIn'} />
         <Stack.Screen component={SignUp} name={'SignUp'} />
         <Stack.Screen component={Home} name={'Home'} />
@@ -39,6 +55,7 @@ export default function Navigator() {
         <Stack.Screen component={Agenda} name={'Agenda'} />
         <Stack.Screen component={PantherProduct} name={'PantherProduct'} />
         <Stack.Screen component={SaveOurSouls} name={'SaveOurSouls'} />
+        <Stack.Screen component={EditUserProfile} name={'EditUserProfile'} />
       </Stack.Navigator>
     </NavigationContainer>
   );

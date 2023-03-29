c0b487ffaff9aeab238151189a009d3aa11720ba
Radiant-F <radiantfadilah0@gmail.com>
2023-03-29 10:45:30 +0700

    test axios

--- src/screens/__test__/LibsTest.test.js
+++ src/screens/__test__/LibsTest.test.js
@@ -1,56 +1,19 @@
-import {StyleSheet, Text, View, Linking} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import MapView, {PROVIDER_GOOGLE, Callout, Marker} from 'react-native-maps';
+import {API_SIGNIN} from '@env';
+import axios from 'axios';
 
 export default function LibsTest() {
-  const userLocations = [
-    {
-      latitude: -7.995307,
-      longitude: 110.296256,
-      whatsapp: 6285157439660,
-      user_name: 'Radiant',
-    },
-    {
-      latitude: -7.997018,
-      longitude: 110.297909,
-      whatsapp: 6285695078232,
-      user_name: 'Jujun',
-    },
-    {
-      latitude: -7.996729,
-      longitude: 110.294103,
-      whatsapp: 6281931314133,
-      user_name: 'Mamah',
-    },
-  ];
+  axios
+    .post(API_SIGNIN, {
+      email: 'testing02@gmail.com',
+      password: 'rahasia123',
+    })
+    .then(res => console.log(res.data))
+    .catch(err => console.log(err));
   return (
-    <View style={{flex: 1}}>
-      <MapView
-        showsCompass
-        showsMyLocationButton
-        style={{flex: 1}}
-        provider={PROVIDER_GOOGLE}
-        initialRegion={{
-          latitude: -7.9964694,
-          longitude: 110.295591,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}>
-        {userLocations.map((v, i) => (
-          <Marker
-            onSelect={() => console.log('on select')}
-            onCalloutPress={() => console.log('on callout press')}
-            onPress={async () =>
-              await Linking.openURL(`https://wa.me/${v.whatsapp}`)
-            }
-            onDeselect={() => console.log('on deselect')}
-            key={i}
-            coordinate={{
-              latitude: v.latitude,
-              longitude: v.longitude,
-            }}></Marker>
-        ))}
-      </MapView>
+    <View>
+      <Text>LibsTest</Text>
     </View>
   );
 }

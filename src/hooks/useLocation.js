48bb73289db5b768b72ceb8e1bc4af6a2951b3e7
Radiant-F <radiantfadilah0@gmail.com>
2023-03-29 15:11:30 +0700

    update return return

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,3 +1,39 @@
-export default function useLocation() {
-  return null;
-}
+import {useEffect, useState} from 'react';
+import Geolocation from 'react-native-geolocation-service';
+
+const useLocation = () => {
+  const [location, setLocation] = useState({
+    accuracy: null,
+    altitude: null,
+    altitudeAccuracy: null,
+    heading: null,
+    // default to Jakarta
+    longitude: 106.82719,
+    latitude: -6.175395,
+    speed: 0,
+  });
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const watchId = Geolocation.watchPosition(
+      pos => {
+        setLocation(pos.coords);
+      },
+      err => {
+        setError(err);
+      },
+      {
+        enableHighAccuracy: true,
+        timeout: 10000,
+        maximumAge: 1000,
+        distanceFilter: 10,
+      },
+    );
+
+    return () => Geolocation.clearWatch(watchId);
+  }, []);
+
+  return {location, error};
+};
+
+export default useLocation;

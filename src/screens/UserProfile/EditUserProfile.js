fddfbd1412bebac7c6dd578d556685a6c5e36610
Radiant-F <radiantfadilah0@gmail.com>
2023-03-29 15:10:43 +0700

    initial commit

---
+++ src/screens/UserProfile/EditUserProfile.js
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function EditUserProfile() {
+  return (
+    <View style={{flex: 1}}>
+      <Text>EditUserProfile</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});

0152449f47e0aa816e5b07dee62ac2c312d74fe8
Radiant-F <radiantfadilah0@gmail.com>
2023-04-01 20:53:44 +0700

    add currency format

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,6 @@
 import useLocation from './useLocation';
 import useOrientation from './useOrientation';
 import usePermission from './usePermission';
+import useCurrencyFormat from './useCurrencyFormat';
 
-export {useLocation, useOrientation, usePermission};
+export {useLocation, useOrientation, usePermission, useCurrencyFormat};

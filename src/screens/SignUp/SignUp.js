1d8a3b7a7990c3127e6e6a40138b8f54ad279ca4
Radiant-F <radiantfadilah0@gmail.com>
2023-04-01 07:55:08 +0700

    update log

--- src/screens/SignUp/SignUp.js
+++ src/screens/SignUp/SignUp.js
@@ -208,7 +208,7 @@ export default function SignUp({navigation}) {
     multiPart.append('sim', formPhotos.sim);
     multiPart.append('stnk', formPhotos.stnk);
 
-    // console.log(email);
+    // console.log(multiPart);
 
     dispatch(fetchSignUp({multiPart, navigation}));
   }

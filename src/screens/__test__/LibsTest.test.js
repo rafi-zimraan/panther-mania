b985d1bb5429fa6f5842066007bd8430b5acb35c
Radiant-F <radiantfadilah0@gmail.com>
2023-04-01 07:52:47 +0700

    test react hook form

--- src/screens/__test__/LibsTest.test.js
+++ src/screens/__test__/LibsTest.test.js
@@ -1,19 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {API_SIGNIN} from '@env';
-import axios from 'axios';
+import FormField from './FormField';
 
 export default function LibsTest() {
-  axios
-    .post('https://panther-mania.id/api/v1/auth', {
-      email: 'testing02@gmail.com',
-      password: 'rahasia123',
-    })
-    .then(res => console.log(res.data))
-    .catch(err => console.log(err));
   return (
     <View>
       <Text>LibsTest</Text>
+      <FormField onSubmit={data => console.log(data)} />
     </View>
   );
 }

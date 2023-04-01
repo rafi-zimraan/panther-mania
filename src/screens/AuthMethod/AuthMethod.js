d154e279092269c22428d70fe55fb02821504cc1
Radiant-F <radiantfadilah0@gmail.com>
2023-04-01 07:53:26 +0700

    update import

--- src/screens/AuthMethod/AuthMethod.js
+++ src/screens/AuthMethod/AuthMethod.js
@@ -1,7 +1,7 @@
 import {StyleSheet, Text, View, Image, Alert} from 'react-native';
 import React from 'react';
 import {ImgBGDefault, ImgPMCar} from '../../assets';
-import useOrientation from '../../utils/useOrientation';
+import useOrientation from '../../hooks/useOrientation';
 import {ButtonAuthMethod} from '../../features/Auth';
 import {Gap} from '../../components';
 export default function AuthMethod({navigation}) {

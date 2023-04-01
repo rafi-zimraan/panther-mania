0683ebc826e336d3853199ba46d03e595839bdd6
Radiant-F <radiantfadilah0@gmail.com>
2023-04-01 07:56:06 +0700

    update permission & add colors constant

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -4,3 +4,10 @@ import {PermissionsAndroid} from 'react-native';
 const Permit = PermissionsAndroid.PERMISSIONS;
 export const AccessFineLocation = Permit.ACCESS_FINE_LOCATION;
 export const AccessCoarseLocation = Permit.ACCESS_COARSE_LOCATION;
+export const PermissionCamera = Permit.CAMERA;
+
+// Colors
+export const colors = {
+  primary: '#183240',
+  destroy: 'tomato',
+};

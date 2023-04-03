4ac00494d4fc4ad6bb92d64c1e2b2c8f803b6638
Radiant-F <radiantfadilah0@gmail.com>
2023-04-03 13:02:52 +0700

    update location & users marker

--- src/screens/SOS/SaveOurSouls.js
+++ src/screens/SOS/SaveOurSouls.js
@@ -8,8 +8,13 @@ import {
   Modal,
 } from 'react-native';
 import React, {useEffect, useState} from 'react';
-import MapView, {Marker, PROVIDER_GOOGLE, Callout} from 'react-native-maps';
-import useLocation from '../../hooks/useLocation';
+import MapView, {
+  Marker,
+  PROVIDER_GOOGLE,
+  Callout,
+  Circle,
+} from 'react-native-maps';
+import {useLocation} from '../../hooks';
 import {ModalLocation} from '../../features/SOS';
 import {useDispatch, useSelector} from 'react-redux';
 import {fetchUsersLocation} from '../../features/SOS/services/sosServices';
@@ -17,7 +22,7 @@ import ModalUserDetail from '../../features/SOS/components/ModalUserDetail';
 import {SetModal} from '../../redux/slices/sosSlice';
 
 export default function SaveOurSouls() {
-  const {location, getCurrentLocation} = useLocation();
+  const {latitude, longitude, loading} = useLocation().location;
   const dispatch = useDispatch();
   const {status, users_data} = useSelector(state => state.save_our_souls);
 
@@ -35,29 +40,23 @@ export default function SaveOurSouls() {
     if (status == 'idle') dispatch(fetchUsersLocation());
   }, [dispatch]);
 
+  const mapRegion = {
+    latitude,
+    longitude,
+    latitudeDelta: 0.05,
+    longitudeDelta: 0.05,
+  };
+
   return (
     <View style={{flex: 1}}>
-      {!location.loading && (
+      {!loading && (
         <MapView
           showsCompass
           showsMyLocationButton
           style={{flex: 1}}
           provider={PROVIDER_GOOGLE}
-          region={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}>
-          {users_data?.length == 0 && (
-            <Text>Sepertinya tidak ada pengguna yang bisa dihubungi..</Text>
-          )}
-          <Marker
-            pinColor="orange"
-            coordinate={{
-              latitude: location.latitude,
-              longitude: location.longitude,
-            }}>
+          region={mapRegion}>
+          <Marker pinColor="dodgerblue" coordinate={{latitude, longitude}}>
             <Callout>
               <Text style={{color: 'black'}}>Anda berada disini</Text>
             </Callout>

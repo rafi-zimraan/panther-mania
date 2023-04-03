3b26baca38a9d3fd9b6f631b198b56e33d9e63a8
Radiant-F <radiantfadilah0@gmail.com>
2023-04-03 15:01:12 +0700

    chores: remove unecessary

--- src/features/Home/components/Agendas.js
+++ src/features/Home/components/Agendas.js
@@ -1,5 +1,4 @@
 import {
-  Image,
   ScrollView,
   StyleSheet,
   Text,
@@ -7,19 +6,15 @@ import {
   View,
   ActivityIndicator,
 } from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
-import {ImgCommunity, ImgShirt} from '../../../assets';
-import {Gap} from '../../../components';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {useNavigation} from '@react-navigation/native';
 import {fetchAgenda} from '../../Agenda/services/agendaServices';
 import {colors} from '../../../utils/constant';
-import {API_KEY_IMAGE} from '@env';
 import AgendaExcerpt from './AgendaExcerpt';
 
 export default function Agendas() {
-  // console.log(API_KEY_IMAGE);
   const dispatch = useDispatch();
   const {navigate} = useNavigation();
   const {status, data} = useSelector(state => state.agenda);
@@ -42,61 +37,10 @@ export default function Agendas() {
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.viewAgenda}>
-        {/* {data?.map((agenda, i) => (
+        {/* AgendaExcerpt to prevent multiple hooks re-render */}
+        {data?.map((agenda, i) => (
           <AgendaExcerpt key={i} data={agenda} />
-        ))} */}
-        {/* {data?.map((v, i) => {
-          // const [chapterImgUri, setChapterImgUri] = useState(
-          //   `${API_KEY_IMAGE}/chapter/${v.chapter_uuid}.jpg`,
-          // );
-          // const [korwilImgUri, setKorwilImgUri] = useState(
-          //   `${API_KEY_IMAGE}/korwil/${v.korwil_uuid}.jpg`,
-          // );
-
-          let chapterImgUri = `${API_KEY_IMAGE}/chapter/${v.chapter_uuid}.jpg`;
-          let korwilImgUri = `${API_KEY_IMAGE}/korwil/${v.korwil_uuid}.jpg`;
-
-          return (
-            <TouchableNativeFeedback
-              key={i}
-              useForeground
-              onPress={() => navigate('AgendaDetail', {agenda: v})}>
-              <View style={styles.btnAgenda}>
-                <View style={styles.viewImgAgenda}>
-                  <Image
-                    source={{uri: chapterImgUri}}
-                    onError={() => {
-                      chapterImgUri = `${API_KEY_IMAGE}/chapter/${v.chapter_uuid}.png`;
-                      // setChapterImgUri(
-                      //   `${API_KEY_IMAGE}/chapter/${v.chapter_uuid}.png`,
-                      // );
-                    }}
-                    style={{width: 100, height: 100}}
-                    resizeMethod={'resize'}
-                  />
-                  <Image
-                    source={{uri: korwilImgUri}}
-                    onError={() => {
-                      korwilImgUri = `${API_KEY_IMAGE}/korwil/${v.korwil_uuid}.png`;
-                      // setKorwilImgUri(
-                      //   `${API_KEY_IMAGE}/korwil/${v.korwil_uuid}.png`,
-                      // );
-                    }}
-                    style={{width: 100, height: 100}}
-                    resizeMethod={'resize'}
-                  />
-                </View>
-                <Text style={styles.textAgendaTitle} numberOfLines={1}>
-                  {v.judul}
-                </Text>
-                <Gap flex={1} />
-                <Text style={styles.textDescription} numberOfLines={2}>
-                  {v.deskripsi}
-                </Text>
-              </View>
-            </TouchableNativeFeedback>
-          );
-        })} */}
+        ))}
         <TouchableNativeFeedback
           useForeground
           onPress={() => navigate('Agenda')}>

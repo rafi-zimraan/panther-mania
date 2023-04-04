d57daf290c5515075d39abea93b9aff69965ae3d
Radiant-F <radiantfadilah0@gmail.com>
2023-04-04 17:09:06 +0700

    update props

--- src/screens/__test__/FormInput.js
+++ src/screens/__test__/FormInput.js
@@ -1,10 +1,40 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View, TextInput} from 'react-native';
+import React, {useState} from 'react';
+import {Picker} from '@react-native-picker/picker';
+import DatePicker from '@react-native-community/datetimepicker';
 
-export default function FormInput() {
+export default function FormInput({
+  onChangeText,
+  value,
+  placeholder,
+  type = 'text',
+  pickerItem,
+  selectedPickerValue,
+  onPickerChange,
+  placeholderPicker = 'Picker Placeholder',
+}) {
+  const [defaultDate, setDefaultDate] = useState(new Date());
+
   return (
     <View>
-      <Text>FormInput</Text>
+      {type == 'text' && (
+        <TextInput
+          onChangeText={onChangeText}
+          value={value}
+          placeholder={placeholder}
+        />
+      )}
+      {type == 'picker' && (
+        <Picker
+          selectedValue={selectedPickerValue}
+          onValueChange={onPickerChange}>
+          <Picker.Item label={placeholderPicker} value={placeholderPicker} />
+          {pickerItem?.map((item, i) => (
+            <Picker.Item label={item.label} value={item.label} key={i} />
+          ))}
+        </Picker>
+      )}
+      {type == 'date' && <DatePicker />}
     </View>
   );
 }

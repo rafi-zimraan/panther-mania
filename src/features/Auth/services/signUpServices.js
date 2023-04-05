3f3d178e04a1a53eafdc9dfe368241ed2871a924
Radiant-F <radiantfadilah0@gmail.com>
2023-04-05 16:03:09 +0700

    update index multipart

--- src/features/Auth/services/signUpServices.js
+++ src/features/Auth/services/signUpServices.js
@@ -12,21 +12,66 @@ const catchError = error => {
   console.log(error);
 };
 
+const form = {
+  _parts: [
+    ['agama', 'Islam'],
+    ['alamat_lengkap', 'Alamat lengkap'],
+    ['alamat_perusahaan', 'alamat perusahaan'],
+    ['email', 'testing102@gmail.com'],
+    ['handphone', '0823475716374'],
+    ['jenis_kelamin', 'Laki-laki'],
+    ['kabupaten_kota', 'kabupaten/kota'],
+    ['kecamatan', 'kecamatan'],
+    ['kelurahan', 'keluarahan'],
+    ['kodepos', '55772'],
+    ['lat', -7.9964723],
+    ['lng', 110.2955627],
+    ['nama_lengkap', 'Nama Lengkap'],
+    ['nama_perusahaan', 'nama perusahaan'],
+    ['no_chasis', 'rrr33444werrr'],
+    ['no_engine', 'ggggghhhhh3333ww3'],
+    ['no_ktp', '222333111000848'],
+    ['no_polisi', 'd3455egh'],
+    ['no_sim', '987484848493999'],
+    ['no_whatsapp', '085157439660'],
+    ['password', 'rahasia123'],
+    ['password_confirmation', 'rahasia123'],
+    ['pekerjaan', 'Programmer'],
+    ['provinsi', 'provinsi'],
+    ['sekolah', 'sekolah'],
+    ['status_nikah', 'Menikah'],
+    ['tahun_kendaraan', '2202'],
+    ['tanggal_lahir', '2023-04-01'],
+    ['tanggal_pajak', '2023-04-26'],
+    ['telp_kantor', '082347583749'],
+    ['telp_rumah', '084758237483'],
+    ['tempat_lahir', 'Bandung'],
+    ['type_kendaraan', 'tipe kendaraan'],
+    ['ukuran_baju', 'M'],
+    ['warna_kendaraan', 'warna kendaraan'],
+    ['profile', [Object]],
+    ['ktp', [Object]],
+    ['bukti_tf', [Object]],
+    ['sim', [Object]],
+    ['stnk', [Object]],
+  ],
+};
+
 export const fetchSignUp = createAsyncThunk(
   'fetchSignUp',
   async ({multiPart, navigation}, {dispatch}) => {
     try {
       const {data: response} = await postSignUp(multiPart, navigation);
       const {message} = response;
-      // console.log(response);
+      // console.log('register:', response);
 
       if (message == 'Data Member berhasil disimpan. Silahkan login') {
-        // 3, 11 & 16 are index position for email, password & nama_lengkap
+        // 3, 20 & 12 are index position for email, password & nama_lengkap
         const email = multiPart._parts[3][1],
-          password = multiPart._parts[17][1],
-          fullName = multiPart._parts[11][1],
-          lat = multiPart._parts[33][1],
-          lng = multiPart._parts[35][1];
+          password = multiPart._parts[20][1],
+          fullName = multiPart._parts[12][1],
+          lat = multiPart._parts[10][1],
+          lng = multiPart._parts[11][1];
 
         const formSignIn = {email, password, lat, lng};
         const {data: dataSignIn} = await postSignIn(formSignIn);
@@ -38,7 +83,7 @@ export const fetchSignUp = createAsyncThunk(
             'Registrasi Berhasil',
             'Menunggu persetujuan registrasi. Harap hubungi admin yang bersangkutan.',
           );
-        // console.log(dataSignIn);
+        // console.log('sign in:', dataSignIn);
 
         await EncryptedStorage.setItem(
           'user_credential',

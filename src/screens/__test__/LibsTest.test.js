c1c42c9056d78b44b5becd7df075a8ae756aab89
Radiant-F <radiantfadilah0@gmail.com>
2023-06-29 19:35:38 +0700

    test form input

--- src/screens/__test__/LibsTest.test.js
+++ src/screens/__test__/LibsTest.test.js
@@ -1,42 +1,55 @@
-import {Button, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
-import {useForm} from 'react-hook-form';
-import {FormInput} from '../../features/Auth';
-import {Gap} from '../../components';
+// import {Button, StyleSheet, Text, View} from 'react-native';
+// import React from 'react';
+// import {useForm} from 'react-hook-form';
+// import {FormInput} from '../../features/Auth';
+// import {Gap} from '../../components';
+
+// export default function LibsTest() {
+//   const {
+//     control,
+//     formState: {errors},
+//     handleSubmit,
+//   } = useForm();
+
+//   return (
+//     <View>
+//       <Text>LibsTest</Text>
+//       <Gap height={20} />
+//       <FormInput
+//         control={control}
+//         errors={errors}
+//         name="profile"
+//         type="image"
+//         title={'Foto Profil'}
+//       />
+//       <Gap height={20} />
+//       <FormInput
+//         control={control}
+//         errors={errors}
+//         name="ktp"
+//         type="image"
+//         title={'Foto KTP'}
+//       />
+//       <Gap height={20} />
+//       <Button
+//         title="Submit"
+//         onPress={handleSubmit(data => console.log(data))}
+//       />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({});
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 export default function LibsTest() {
-  const {
-    control,
-    formState: {errors},
-    handleSubmit,
-  } = useForm();
-
   return (
     <View>
-      <Text>LibsTest</Text>
-      <Gap height={20} />
-      <FormInput
-        control={control}
-        errors={errors}
-        name="profile"
-        type="image"
-        title={'Foto Profil'}
-      />
-      <Gap height={20} />
-      <FormInput
-        control={control}
-        errors={errors}
-        name="ktp"
-        type="image"
-        title={'Foto KTP'}
-      />
-      <Gap height={20} />
-      <Button
-        title="Submit"
-        onPress={handleSubmit(data => console.log(data))}
-      />
+      <Text>LibsTest.test</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({})
